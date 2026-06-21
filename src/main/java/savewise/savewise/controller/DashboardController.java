749df2a7844d5558f26f8ef5ae116e7e3958555f
marcpons4 <marponlac@gmail.com>
2026-06-21 17:15:44 +0200

    Transacciones

--- src/main/java/savewise/savewise/controller/DashboardController.java
+++ src/main/java/savewise/savewise/controller/DashboardController.java
@@ -1,18 +1,49 @@
 package savewise.savewise.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import savewise.savewise.entity.User;
+import savewise.savewise.repository.CategoryRepository;
+import savewise.savewise.repository.FinancialTransactionRepository;
+import savewise.savewise.repository.UserRepository;
+
 @Controller
 public class DashboardController {
 
-    @GetMapping("/dashboard")
-    public String dashboard(Model model) {
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final FinancialTransactionRepository transactionRepository;
 
-        model.addAttribute("income", 0);
-        model.addAttribute("expenses", 0);
-        model.addAttribute("balance", 0);
+    public DashboardController(
+            UserRepository userRepository,
+            CategoryRepository categoryRepository,
+            FinancialTransactionRepository transactionRepository) {
+
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(
+            Model model,
+            Principal principal) {
+
+        User user = userRepository
+                .findByUsername(principal.getName())
+                .orElseThrow();
+
+        model.addAttribute(
+                "totalCategories",
+                categoryRepository.findByUser(user).size());
+
+        model.addAttribute(
+                "totalTransactions",
+                transactionRepository.findByUser(user).size());
 
         return "dashboard";
     }
--- src/main/java/savewise/savewise/repository/FinancialTransactionRepository.java
+++ src/main/java/savewise/savewise/repository/FinancialTransactionRepository.java
@@ -1,11 +1,16 @@
 package savewise.savewise.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import savewise.savewise.entity.FinancialTransaction;
+import savewise.savewise.entity.User;
 
 @Repository
 public interface FinancialTransactionRepository
         extends JpaRepository<FinancialTransaction, Long> {
+
+    List<FinancialTransaction> findByUser(User user);
 }
